c27fb0aafc53bb782252291c4aa3006a14265d1b
flamingpsyduck <adelynelizabethpierce@gmail.com>
2025-05-08 23:19:34 -0400

    major update

---
+++ index.js
@@ -0,0 +1,14 @@
+$( function() {
+    $( document ).tooltip({
+      items: "img, [title]",
+      content: function() {
+        var element = $( this );
+        if ( element.is( "[title]" ) ) {
+          return element.attr( "title" );
+        }
+        if ( element.is( "img" ) ) {
+          return element.attr( "alt" );
+        }
+      }
+    });
+  } );
